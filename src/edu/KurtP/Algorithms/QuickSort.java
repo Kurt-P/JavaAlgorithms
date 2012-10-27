6dda8d19d26e4942781c36896ae72b44d216467b
Kurt Prudhomme <krprudhomme@gmail.com>
2012-10-27 14:35:57 -0500

    Commit to push

---
+++ src/edu/KurtP/Algorithms/QuickSort.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.KurtP.Algorithms;
+
+/**
+ *
+ * @author kurtprudhomme
+ */
+public class QuickSort {
+    
+    public static int[] sort(int[] inputArray) {
+        int[] array = inputArray;
+        
+        
+        
+        return array;
+    }
+}
--- src/edu/KurtP/Algorithms/SelectionSort.java
+++ src/edu/KurtP/Algorithms/SelectionSort.java
@@ -5,24 +5,37 @@ package edu.KurtP.Algorithms;
  * @version 1.0.10252012
  */
 public class SelectionSort {
-    
+
+    /**
+     * Sorts the inputted array.
+     *
+     * @param inputArray
+     * @return The sorted array.
+     */
     public static int[] sort(int[] inputArray) {
+        //The array variable
         int[] array = inputArray;
         int k, j;
-        
+
+        //The array will be looped through
         for (int i = 0; i < array.length - 1; i++) {
             k = i;
+            //The values of the K will be compared to the values of J.
             for (j = i + 1; j < array.length; j++) {
                 if (array[j] < array[k]) {
                     k = j;
                 }
             }
+
+            //Swap I with K
             if (k != i) {
                 int temp = array[i];
                 array[i] = array[k];
                 array[k] = temp;
             }
         }
+
+        //Return the sorted array.
         return array;
     }
 }
